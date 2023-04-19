541b6f11c0dff46aaeda0bd2616666dc7e715fe1
Michael Householder <householdcodermike@gmail.com>
2023-04-18 21:00:15 -0400

    just an update
    pog

--- FinalProj.m
+++ FinalProj.m
@@ -2,27 +2,40 @@ FISH1 = imread("FISH1.png");
 FISH2 = imread("FISH2.png");
 FISH3 = imread("FISH3.png");
 
-resizedFISH1 = imresize(FISH1, [341, 512]);
-resizedFISH2 = imresize(FISH2, [341, 512]);
-resizedFISH3 = imresize(FISH3, [341, 512]);
+% resizedFISH1 = imresize(FISH1, [341, 512]);
+% resizedFISH2 = imresize(FISH2, [341, 512]);
+% resizedFISH3 = imresize(FISH3, [341, 512]);
 
-grayFISH1 = rgb2gray(FISH1);
-grayFISH2 = rgb2gray(FISH2);
-grayFISH3 = rgb2gray(FISH3);
+phatFISH1 = imtophat(FISH1,strel('disk', 200));
+% imshow(phatFISH1);
 
-level1 = graythresh(grayFISH1);
-level2 = graythresh(grayFISH2);
-level3 = graythresh(grayFISH3);
-
+grayPHATFISH1 = rgb2gray(phatFISH1);
+correctedFISH1 = imadjust(grayPHATFISH1);
+imshow(correctedFISH1);
 
 
-BW1 = imbinarize(grayFISH1, level1);
-BW2 = imbinarize(grayFISH2, level2);
-BW3 = imbinarize(grayFISH3, level3);
 
-figure(1);
-imshowpair(FISH1, BW1, 'montage');
-figure(2);
-imshowpair(FISH2, BW2, 'montage');
-figure(3);
-imshowpair(FISH3, BW3, 'montage');
+bw = imbinarize(correctedFISH1);
+bw = bwareaopen(bw,50);
+% imshow(bw);
+
+idfkFISH = grayPHATFISH1 - bw;
+imshow(idfkFISH);
+% grayFISH1 = rgb2gray(FISH1);
+% grayFISH2 = rgb2gray(FISH2);
+% grayFISH3 = rgb2gray(FISH3);
+% 
+% level1 = graythresh(grayFISH1);
+% level2 = graythresh(grayFISH2);
+% level3 = graythresh(grayFISH3);
+% 
+% BW1 = imbinarize(grayFISH1, level1);
+% BW2 = imbinarize(grayFISH2, level2);
+% BW3 = imbinarize(grayFISH3, level3);
+% 
+% figure(1);
+% imshowpair(FISH1, BW1, 'montage');
+% figure(2);
+% imshowpair(FISH2, BW2, 'montage');
+% figure(3);
+% imshowpair(FISH3, BW3, 'montage');
